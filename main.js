85ea089b338c1258271a06be34bce6518aee6adc
hknktk <75674444+hknktk@users.noreply.github.com>
2021-03-28 17:34:22 +0900

    commit
    更新した。

--- main.js
+++ main.js
@@ -1,32 +1,39 @@
 {
-  const $submit = document.getElementById('submit');
-  const $container = document.getElementById('container');
-  const $box1 = document.querySelector('.box1');
-  const $box2 = document.querySelector('.box2');
-  const $box3 = document.querySelector('.box3');
-  const $box4 = document.querySelector('.box4');
-  
-  
+  const $submit = document.getElementById("submit");
+  const $container = document.getElementById("container");
+  const $box1 = document.querySelector(".box1");
+  const $box2 = document.querySelector(".box2");
+  const $box3 = document.querySelector(".box3");
+  const $box4 = document.querySelector(".box4");
+  const $not_grid = document.getElementById("not_grid");
+  let $width = document.getElementById("width");
+  let $height = document.getElementById("height");
 
-
-  $submit.addEventListener('click',()=>{
-    const $display = document.getElementById('display');
-    const $display_value = $display.value;
+  $submit.addEventListener("click", () => {
+    let $display = document.getElementById("display");
+    let $display_value = $display.value;
     $container.style.display = $display_value;
 
-    const $width = document.getElementById('width');
-    const $width_value = $width.value;
-    $box1.style.width = $width_value + "%";
-    $box2.style.width = $width_value + "%";
-    $box3.style.width = $width_value + "%";
-    $box4.style.width = $width_value + "%";
+    if ($container.style.display === "grid") {
+      console.log("grid");
+      $not_grid.style.display = "none";
+    } else {
+      $not_grid.style.display = "inline";
+      let $width = document.getElementById("width");
+      let $width_value = $width.value;
 
-    const $height = document.getElementById('height');
-    const $height_value = $height.value;
-    $box1.style.height = $height_value + "px";
-    $box2.style.height = $height_value + "px";
-    $box3.style.height = $height_value + "px";
-    $box4.style.height = $height_value + "px";
-  })
+      $box1.style.width = $width_value + "%";
+      $box2.style.width = $width_value + "%";
+      $box3.style.width = $width_value + "%";
+      $box4.style.width = $width_value + "%";
 
+      let $height = document.getElementById("height");
+      let $height_value = $height.value;
+
+      $box1.style.height = $height_value + "px";
+      $box2.style.height = $height_value + "px";
+      $box3.style.height = $height_value + "px";
+      $box4.style.height = $height_value + "px";
+    }
+  });
 }
