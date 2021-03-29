fbc1534130abf531bcf299215bace8ebbb721ee2
hknktk <75674444+hknktk@users.noreply.github.com>
2021-03-30 00:18:02 +0900

    commit
    更新した。

--- main.js
+++ main.js
@@ -6,7 +6,7 @@
   const $box3 = document.querySelector(".box3");
   const $box4 = document.querySelector(".box4");
   const $not_grid = document.getElementById("not_grid");
-  const $grid = document.getElementById('grid');
+  const $grid = document.getElementById("grid");
   const $flex = document.getElementById("flex");
 
   let $width = document.getElementById("width");
@@ -18,26 +18,61 @@
     $container.style.display = $display_value;
 
     if ($container.style.display === "grid") {
-      let $width = document.getElementById("width");
-      $width.style.width = "unset";
-      let $height = document.getElementById("width");
-      $height.style.width = "unset";
-      $not_grid.style.display = "none";
-      $flex.style.display = "none";
-      $grid.style.display = "inline";
       $container.style.flexDirection = "unset";
       $container.style.justifyContent = "unset";
       $container.style.alignItems = "unset";
 
+      $not_grid.style.display = "none";
+      $flex.style.display = "none";
+      $grid.style.display = "inline";
 
+      let $grid_template_columns = document.getElementById(
+        "grid-template-columns"
+      );
+      let $grid_template_columns_value = $grid_template_columns.value;
+      $container.style.gridTemplateColumns = $grid_template_columns_value;
 
+      let $grid_template_rows = document.getElementById("grid-template-rows");
+      let $grid_template_rows_value = $grid_template_rows.value;
+      $container.style.gridTemplateRows = $grid_template_rows_value;
+
+      let $grid_column1 = document.getElementById("b1_grid-column");
+      let $grid_column1_value = $grid_column1.value;
+      $box1.style.gridColumn = $grid_column1_value;
+
+      let $grid_column2 = document.getElementById("b2_grid-column");
+      let $grid_column2_value = $grid_column2.value;
+      $box2.style.gridColumn = $grid_column2_value;
+
+      let $grid_column3 = document.getElementById("b3_grid-column");
+      let $grid_column3_value = $grid_column3.value;
+      $box3.style.gridColumn = $grid_column3_value;
+
+      let $grid_column4 = document.getElementById("b4_grid-column");
+      let $grid_column4_value = $grid_column4.value;
+      $box4.style.gridColumn = $grid_column4_value;
+
+      let $grid_row1 = document.getElementById("b1_grid-row");
+      let $grid_row1_value = $grid_row1.value;
+      $box1.style.gridRow = $grid_row1_value;
+
+      let $grid_row2 = document.getElementById("b2_grid-row");
+      let $grid_row2_value = $grid_row2.value;
+      $box2.style.gridRow = $grid_row2_value;
+
+      let $grid_row3 = document.getElementById("b3_grid-row");
+      let $grid_row3_value = $grid_row3.value;
+      $box3.style.gridRow = $grid_row3_value;
+
+      let $grid_row4 = document.getElementById("b4_grid-row");
+      let $grid_row4_value = $grid_row4.value;
+      $box4.style.gridRow = $grid_row4_value;
     } else if ($container.style.display === "flex") {
       $flex.style.display = "inline";
       $grid.style.display = "none";
       let $flex_direction = document.getElementById("flex-direction");
       let $flex_direction_value = $flex_direction.value;
       $container.style.flexDirection = $flex_direction_value;
-      
 
       let $justify_content = document.getElementById("justify-content");
       let $justify_content_value = $justify_content.value;
@@ -84,6 +119,5 @@
       $box3.style.height = $height_value + "px";
       $box4.style.height = $height_value + "px";
     }
-
   });
 }
