bab54bb3dc53103ec70321d72a9cf60900021132
hknktk <75674444+hknktk@users.noreply.github.com>
2021-03-29 11:00:15 +0900

    commit
    更新した。

--- main.js
+++ main.js
@@ -16,14 +16,40 @@
 
     if ($container.style.display === "grid") {
       $not_grid.style.display = "none";
-    } 
-    else if($container.style.display === "flex") {
-      let $flex = document.getElementById('flex');
+    } else if ($container.style.display === "flex") {
+      let $flex = document.getElementById("flex");
       $flex.style.display = "inline";
-      let $flex_direction = document.getElementById('flex-direction');
+      let $flex_direction = document.getElementById("flex-direction");
       let $flex_direction_value = $flex_direction.value;
       $container.style.flexDirection = $flex_direction_value;
 
+      let $justify_content = document.getElementById("justify-content");
+      let $justify_content_value = $justify_content.value;
+      $container.style.justifyContent = $justify_content_value;
+
+      let $align_items = document.getElementById("align-items");
+      let $align_items_value = $align_items.value;
+      $container.style.alignItems = $align_items_value;
+
+      $not_grid.style.display = "inline";
+      let $width = document.getElementById("width");
+      let $width_value = $width.value;
+
+      $box1.style.width = $width_value + "%";
+      $box2.style.width = $width_value + "%";
+      $box3.style.width = $width_value + "%";
+      $box4.style.width = $width_value + "%";
+
+      let $height = document.getElementById("height");
+      let $height_value = $height.value;
+
+      $box1.style.height = $height_value + "px";
+      $box2.style.height = $height_value + "px";
+      $box3.style.height = $height_value + "px";
+      $box4.style.height = $height_value + "px";
+    } else {
+      let $flex = document.getElementById("flex");
+      $flex.style.display = "none";
 
       $not_grid.style.display = "inline";
       let $width = document.getElementById("width");
@@ -42,7 +68,5 @@
       $box3.style.height = $height_value + "px";
       $box4.style.height = $height_value + "px";
     }
-
-    
   });
 }
